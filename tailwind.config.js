e1ce170f02e315e6d8013fab0e2cc3693864c41e
DADXUS <cesarruizvidaurre@gmail.com>
2026-05-12 22:47:55 -0500

    Limpieza de tailwind v3 a v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,7 @@ export default {
     "./resources/**/*.js",
   ],
   theme: {
-    extend: {
-      colors: {
-        // Estos son los colores 
-        'obscure': '#111111', 
-        'obscure-darker': '#050505',
-        'obscure-light': '#111111',
-        'obscure-lightest': '#222222',
-        'emph': '#2563eb', // El azul de tus botones
-        'clarity': '#9ca3af',
-        'clarity-lighter': '#f3f4f6',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
