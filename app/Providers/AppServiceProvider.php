ebb1a92d0970b35f9d11388a8e3f2af27fcb2a74
DADXUS <cesarruizvidaurre@gmail.com>
2026-05-12 22:28:00 -0500

    Funcition Boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
-    {
-        //
+{
+    if (config('app.env') === 'production') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
+}
 }
