6a59c0e8305872612a3e9d15bd4a93026ed7241e
I0nKxrry <jhonkerryrivearojas@mail.com>
2026-05-14 16:17:17 -0500

    traducción de footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,25 +2,25 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-12 mb-10">
         <div>
             <p class="text-2xl font-bold mb-6">EMPHERATOR</p>
-            <p class="text-clarity text-sm max-w-xs">Building tools for the next generation. High-performance, zero compromise.</p>
+            <p class="text-clarity text-sm max-w-xs">Desarrollando herramientas para la próxima generación. Alto rendimiento, sin concesiones.</p>
         </div>
         <div>
-            <p class="font-bold mb-4 uppercase text-xs tracking-widest">Support</p>
+            <p class="font-bold mb-4 uppercase text-xs tracking-widest">Soporte</p>
             <div class="flex flex-col gap-2 text-sm text-clarity">
-                <a href="#">Tech Support</a>
-                <a href="#">Downloads</a>
+                <a href="#">Soporte Técnico</a>
+                <a href="#">Descargas</a>
             </div>
         </div>
         <div>
-            <p class="font-bold mb-4 uppercase text-xs tracking-widest">Company</p>
+            <p class="font-bold mb-4 uppercase text-xs tracking-widest">Compañia</p>
             <div class="flex flex-col gap-2 text-sm text-clarity">
-                <a href="#">Contact</a>
-                <a href="#">Global Shipping</a>
+                <a href="#">Contacto</a>
+                <a href="#">Compra global</a>
             </div>
         </div>
     </div>
     <div class="pt-8 border-t border-obscure-lightest text-[10px] text-gray-600 flex flex-col items-center gap-2">
-        <p>GRUPO N# TECNOLOGIAS WEB UNT 2026-1</p>
+        <p>GRUPO N# TECNOLOGIAS WEB UNT 2026-I</p>
         <p>Gerardo Venegas | Cesar Ruiz | Kerry Rivera | Eduardo Torres</p>
     </div>
 </footer>
