549596ee2a92fa51366d90fd34927fea73018b9f
DADXUS <cesarruizvidaurre@gmail.com>
2026-05-14 18:13:53 -0500

    Mejora

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,7 +36,7 @@ class ProductController extends Controller
             'id' => 4,
             'name' => 'Disco SSD 1 TB Predator GM6 PCle Gen4',
             'category' => 'COMPONENTES',
-            'price' => 549,
+            'price' => 549.90,
             'image' => 'imagenes/productos/componentes/Disco SSD 1 TB Predator GM6 PCle Gen4.jpg',
             'description' => 'El SSD Predator GM6 1TB es una unidad NVMe PCIe Gen4 x4 M.2 2280 de alto rendimiento diseñada para gaming, edición de video y PS5'
         ],
@@ -58,18 +58,18 @@ class ProductController extends Controller
         ],
         [
             'id' => 7,
-            'name' => 'K-900 SPECTRAL',
-            'category' => 'COMPONENTES',
-            'price' => 249,
-            'image' => 'imagenes/productos/componentes/k-900-spectral.png',
+            'name' => 'Wireless 2.4G Peso 70g USB‑C',
+            'category' => 'MOUSE',
+            'price' => 249.90,
+            'image' => 'imagenes/productos/mouse/Wireless 2.4G Peso 70g USB‑C.jpeg',
             'description' => 'Engineered for the absolute edge. Zero-latency mechanical switches encased in an aircraft-grade aluminum chassis.'
         ],
         [
             'id' => 8,
-            'name' => 'M-PRIME MOUSE',
-            'category' => 'AUDIO',
+            'name' => 'FPS Lite Batería 60h Sensor 16K',
+            'category' => 'MOUSE',
             'price' => 159,
-            'image' => 'imagenes/productos/audio/m-prime-mouse.png',
+            'image' => 'imagenes/productos/mouse/FPS Lite Batería 60h Sensor 16K.jpeg',
             'description' => '26,000 DPI Sensor | 54g Total Weight. The new standard for competitive precision.'
         ],
     ];
